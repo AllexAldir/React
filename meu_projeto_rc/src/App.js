c9dcd5e8b3d305f39b7d877d541b476c4e11422d
Allex Aldir <114629926+AllexAldir@users.noreply.github.com>
2023-03-04 17:43:39 -0300

    Criando React

---
+++ meu_projeto_rc/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <p>Alterando o código</p>
+    </div>
+  );
+}
+
+export default App;
