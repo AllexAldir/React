1c42060cf3e41167900849278cd4a2d443782151
Allex Aldir <114629926+AllexAldir@users.noreply.github.com>
2023-03-05 00:29:13 -0300

    Meu primeiro componente!

--- meu_projeto_rc/src/App.js
+++ meu_projeto_rc/src/App.js
@@ -1,12 +1,44 @@
 import './App.css';
+//Aqui vai servir para importar coisas 
+import Hello_World from './components/Helllo_World'
+
+import Teste_final from './components/Teste_Final'
 
 function App() {
+  /*
+    Aqui nessa função aonde declaramos as variáveis do nosso scripts
+  */
+
+  /*
+    Aqui é o nosso arquivo JSX que por baixo dos panos está sobrepondo um arquivo em JS
+  */
+
+  const name = 'Állex'.toLocaleUpperCase()
+  const soma = (x,y) => x + y
+  
+  const img = 'http://via.placeholder.com/150'
+  //Utilizando uma imagem dinâmica 
+
+
   return (
     <div className="App">
       <h1>Hello World</h1>
       <p>Alterando o código</p>
+      <p>Olá {name}</p> 
+      {/* /* aqui posso inporlar com variáveis utilizando {} */ }
+      <p>Soma: {soma(4,5)}</p>
+      <div>
+      <img src={img} alt="Minha imagem"></img>
+      
+      </div>
+
+      {/* componentes importados */}
+      <Hello_World/>
+      
+
     </div>
   );
 }
 
 export default App;
+//Mandando nosso script
---
+++ meu_projeto_rc/src/components/Helllo_World.js
@@ -0,0 +1,15 @@
+import Teste_final from './Teste_Final'
+
+
+function Hello_World(){
+    
+    return(
+        <div>
+            <Teste_final />
+            <h1>Meu primeiro componente</h1>
+            <Teste_final/>
+        </div>
+    )   
+
+}
+export default Hello_World
